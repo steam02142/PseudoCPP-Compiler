66a5f9b656a508027488ea7220f031ff60fb87f6
steam02142 <spencerjmuise@protonmail.com>
2025-03-18 10:30:50 -0700

    + Tokenizer recognizes all valid keywords, not just SET

--- src/tokenizing.cpp
+++ src/tokenizing.cpp
@@ -7,12 +7,11 @@ int tokenize(token tokens[])
 
     cout << "Beginning scanning:\n\n";
 
-    while (cin.good()) {
-        cin >> skipws >> input;
-
+    while (cin >> skipws >> input) {
         if (hanldeKeyword(input, tokens[pos], pos)){
             continue; // move to next token
         }
+        
         
     }
 
@@ -21,9 +20,11 @@ int tokenize(token tokens[])
 
 bool hanldeKeyword(string input, token& token, int& pos)
 {
+    cout << "input " << input << endl; 
     for (unsigned int keypos = Function; keypos <= EndFunction; keypos++) {
         string keyword = ttypeTostr(keypos);
         if (keyword == input) {
+            cout << "found" << endl;
             storeToken(token, keypos, input, pos);
             return  true;
         }
@@ -45,9 +46,22 @@ void storeToken(token& token, unsigned int ttype, string content, int& pos)
 string ttypeTostr(unsigned int ttype)
 {
     switch (ttype) {
+        case Function: return "FUNCTION";
+        case Read: return "READ";
+        case Print: return "PRINT";
+        case Compute: return "COMPUTE";
         case Set: return "SET";
+        case Call: return "CALL";
+        case Increment: return "INCREMENT";
+        case Decrement: return "DECREMENT";
+        case Add: return "ADD"; 
+        case Sub: return "SUB"; 
+        case Mul: return "MUL"; 
+        case Div: return "DIV"; 
+        case EndFunction: return "ENDFUNCTION";
         default: return "Invalid";
-    }
+}
+
 }
 
 void printTokens(token tokens[], int size)
